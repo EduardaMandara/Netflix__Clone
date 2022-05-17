56089dfb03ed794aab978141d7e43a2143b15e1a
EduardaBezerra <eduardaf.bezerra@outlook.com>
2022-05-17 15:26:20 -0300

    update the folder

---
+++ netflixclone/src/Tmdb.js
@@ -0,0 +1,15 @@
+const API_KEY = '38cee7f28d5b66f36b9c3cf8d8452a4b';
+const API_BASE = 'https://api.themoviedb.org/3';
+
+/* 
+- Originais da Netflix
+- recomendados (tranding)
+- em alta (top rated)
+- 
+*/
+
+export default {
+ return (
+
+ )
+} ;
