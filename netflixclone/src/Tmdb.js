93bc0f54fe4e4a3f4173bd033d19d3eabc0505df
EduardaBezerra <eduardaf.bezerra@outlook.com>
2022-05-17 22:41:51 -0300

    array

--- netflixclone/src/Tmdb.js
+++ netflixclone/src/Tmdb.js
@@ -3,13 +3,71 @@ const API_BASE = 'https://api.themoviedb.org/3';
 
 /* 
 - Originais da Netflix
-- recomendados (tranding)
+- recomendados (trending)
 - em alta (top rated)
-- 
+- ação
+- comédia
+- terror
+- romance
+- documentários
 */
 
-export default {
- return (
+const basicFetch = async (endpoint) => {
+    const req = await fetch('$(API_BASE)$(endpoint)');
+    const json = await req.json();
+    return json;
+}
 
- )
+export default {
+ getHomeList: async () => {
+     return [
+        {
+             slug: 'originals',
+             title: 'Originais da Netflix',
+             items: await basicFetch('/discover/tv?with_network=213'),
+        },
+
+        {
+             slug: 'trending',
+             title: 'Recomendados para Você',
+             items: [],
+        },
+
+        {
+            slug: 'toprated',
+            title: 'Em Alta',
+            items: [],
+        },
+
+        {
+            slug: 'action',
+            title: 'Ação',
+            items: [],
+        }, 
+        
+        {
+            slug: 'comedy',
+            title: 'Comédia',
+            items: [],
+        },
+
+        {
+            slug: 'horror',
+            title: 'Terror',
+            items: [],
+        },
+
+        {
+            slug: 'romance',
+            title: 'Romance',
+            items: [],
+        },
+        
+        {
+            slug: 'documentary',
+            title: 'Documentários',
+            items: [],
+        },
+     ]
+ }
 } ;
